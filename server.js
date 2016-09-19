1ffd324a1d01da6a4470c0ba1eda92edf5965605
addy96 <aditya.wt@gmail.com>
2016-09-19 10:04:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/download.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'download.png'));
+app.get('/ui/harry.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'harry.png'));
 });
 
 
