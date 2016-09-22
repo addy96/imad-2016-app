cfa341f83466304baa8c825206b34a1fe2184e59
addy96 <aditya.wt@gmail.com>
2016-09-22 12:53:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 
-app.get('/', function (req, res) {
+app.get('/go.html, function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'go.html'));
 });
 
