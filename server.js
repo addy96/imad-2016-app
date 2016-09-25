e0db9efd95abca84f5a6ea8440f46cc18a302dfe
addy96 <aditya.wt@gmail.com>
2016-09-25 11:52:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,14 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var content = {
+    title:'Deathly Hallows | Addy',
+    heading::'deathly hallows',
+    date:'sep 25,2016',
+    content:''
+    
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
