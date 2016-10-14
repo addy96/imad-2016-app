5aad7ccdfea1344f6a09f2d8f32aac8e8228edd4
addy96 <aditya.wt@gmail.com>
2016-10-14 16:36:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,2 +1,6 @@
 console.log('Loaded!');
 alert('welcome to Led zeppelin world, click OK');
+var icon = document.getElementById('move');
+icon.onhover = function(){
+    img.style.marginDown = '20px';
+};
