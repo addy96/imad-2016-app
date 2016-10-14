5656f8f674e0f3bd3a38d2934315283939ddbb8d
addy96 <aditya.wt@gmail.com>
2016-10-14 15:42:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -138,6 +138,9 @@ app.get('/ui/:ledName', function (req, res) {
 app.get('/ui/main.js',function(req,res){
     res.senFile(path.join(__dirname,'ui','main.js'));
 });
+app.get('http://cufon.shoqolate.com/js/cufon-yui.js',function(req,res){
+    res.senFile(path.join('led-zeppelin-ii.cufonfonts.js'));
+});
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
