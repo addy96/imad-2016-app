d17dd9a9e2fc6749459fa4c29448bbd2002af8f7
addy96 <aditya.wt@gmail.com>
2016-10-23 21:21:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -17,12 +17,8 @@ button.onclick = function() {
     request.open('GET','http://addy96.imad.hasura-app.io/counter',true);
     request.send(null);
     
-    
-    
-    
-    
-    
-    
-    
 };
+    var nameInput = document.getElementById('name');
+    var name = nameInput.value;
+    var submit = document.getElementById('submit_btn')
 
