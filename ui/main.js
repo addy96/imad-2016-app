0fade4ad7fda831e2e8f07aa14682f39292db34b
addy96 <aditya.wt@gmail.com>
2016-10-24 01:21:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -27,7 +27,7 @@ button.onclick = function() {
       var name= ['name1','name2','name3'];
       var list="";
       for(var i=0;i<names.length;i++){
-          list= list+ '<li>' + names[i] +'</li>';
+          list += '<li>' + names[i] +'</li>';
       }
       
       var ul=document.getElementById('namelist');
