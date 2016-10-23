e1865cfc7241bde6eb1b4dbc3c1e8a7b5d19ea31
addy96 <aditya.wt@gmail.com>
2016-10-24 01:41:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -138,6 +138,11 @@ app.get('/ui/:ledName', function (req, res) {
     var ledName = req.params.ledName;
   res.send(createTemplate(ledzep[ledName]));
 });
+app.get('/submit-name/:name', function (req, res) {
+    var name=req.params.name;
+    names.push(name);
+  res.send(JSON.stringify(names));
+});
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
