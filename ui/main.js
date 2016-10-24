4a1e1bb6caba8ca4e6722215261770821c620d07
addy96 <aditya.wt@gmail.com>
2016-10-24 15:40:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -22,9 +22,14 @@ button.onclick = function() {
     var name = nameInput.value;
     var submit = document.getElementById('submit_btn');
     submit.onclick=function(){
-      
-      
-      var names= ['name1','name2','name3','name4'];
+        
+    var request= new XMLHttpRequest();
+    
+    request.onreadystatechange = function(){
+        if(request.readyState===XMLHttpRequest.DONE) {
+            if(request.status===200){
+                 var names= request.responseText;
+                 names=JSON.parse(name);
       var list='';
       for(var i=0;i<names.length;i++){
           list += '<li>' + names[i] +'</li>';
@@ -32,5 +37,19 @@ button.onclick = function() {
       
       var ul=document.getElementById('namelist');
       ul.innerHTML=list;
+                
+            }
+            
+        }
     };
+    
+    
+    request.open('GET','http://addy96.imad.hasura-app.io/submit-name?name='+ name,true);
+    request.send(null);
+    
+};
+      
+      
+     
+
 
