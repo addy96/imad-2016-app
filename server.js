723e3aaa88002712c6905fc79b959e9edd0c21a7
addy96 <aditya.wt@gmail.com>
2016-10-24 15:02:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -139,8 +139,8 @@ app.get('/ui/:ledName', function (req, res) {
   res.send(createTemplate(ledzep[ledName]));
 });
 var names=[];
-app.get('/submit-name/:name', function (req, res) {
-    var name=req.params.name;
+app.get('/submit-name/', function (req, res) {
+    var name=req.query.name;
     names.push(name);
   res.send(JSON.stringify(names));
 });
