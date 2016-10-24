23ae9cf52d9084b2e3d81afa3d0d68c84c99c688
addy96 <aditya.wt@gmail.com>
2016-10-24 15:43:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -29,7 +29,7 @@ button.onclick = function() {
         if(request.readyState===XMLHttpRequest.DONE) {
             if(request.status===200){
                  var names= request.responseText;
-                 names=JSON.parse(name);
+                 names=JSON.parse(names);
       var list='';
       for(var i=0;i<names.length;i++){
           list += '<li>' + names[i] +'</li>';
