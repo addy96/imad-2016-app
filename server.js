13dac48c8bfc62e560c0cea9334cca21325fa0ff
addy96 <aditya.wt@gmail.com>
2016-11-02 00:33:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -115,9 +115,7 @@ app.get('/', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
-app.get('/ui/Go.html', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'Go.html'));
-});
+
 
 counter=0;
 app.get('/counter',function(req,res){
@@ -131,6 +129,9 @@ app.get('/submit-name', function (req, res) {
     var name = req.query.name;
     names.push(name);
   res.send(JSON.stringify(names));
+});
+app.get('/ui/Go.html', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'Go.html'));
 });
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
